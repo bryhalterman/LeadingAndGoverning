963c30a96d03bab01e6ff34bea05e1cd4e73fe8f
Bryan Halterman <bryhalterman@gmail.com>
2017-08-30 21:02:16 -0400

    WIP
    Created Scripts, CSS, and initial HTML files

---
+++ LeadingAndGoverning/LeadingAndGoverning/Scripts/Site.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="D:\Git\LeadingAndGoverning\LeadingAndGoverning\LeadingAndGoverning\Shared/Footer.html" />
+/// <reference path="D:\Git\LeadingAndGoverning\LeadingAndGoverning\LeadingAndGoverning\Shared/Header.html" />
+/// <reference path="D:\Git\LeadingAndGoverning\LeadingAndGoverning\LeadingAndGoverning\Shared/Navigation.html" />
+var paths = {
+    header: "/Shared/Header.html",
+    footer: "/Shared/Footer.html"
+};
+
+$(document).ready(function () {
+    // Load Header
+    $("header").load(paths.header);
+    $("header").data("welcome").parent().addClass("active")
+    // Load Footer
+    $("footer").load(paths.footer);
+    site.loadContent();
+});
+
+var site = {
+    loadContent: function () {
+        $("nav").first
+    },
+};

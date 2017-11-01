7f00af7dd6ab3257851376945256c797e4f295cd
Bryan Halterman <bryhalterman@gmail.com>
2017-11-01 19:21:15 -0400

    Fixes
    Corrected Project Name from LearningAndGoverning to LeadingAndGoverning.

---
+++ LeadingAndGoverning/LeadingAndGoverning/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using LeadingAndGoverning.Services;
+using LeadingAndGoverning.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeadingAndGoverning.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
